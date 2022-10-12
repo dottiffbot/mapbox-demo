124034d034e9fc3c67233df02f7674042129598a
tuffany <thetifftopor@gmail.com>
2022-10-12 17:51:57 -0400

    cooler example

--- public/script.js
+++ public/script.js
@@ -6,15 +6,35 @@ async function mapSetup(){
     // console.log(token)
 mapboxgl.accessToken = data.token;
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9, // starting zoom
-    projection: 'globe' // display the map as a 3D globe
-});
-map.on('style.load', () => {
-    map.setFog({}); // Set the default atmosphere style
-});
+    container: 'map',
+    zoom: 13.1,
+    center: [-114.34411, 32.6141],
+    pitch: 85,
+    bearing: 80,
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox-map-design/ckhqrf2tz0dt119ny6azh975y'
+    });
+     
+    map.on('load', () => {
+    map.addSource('mapbox-dem', {
+    'type': 'raster-dem',
+    'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
+    'tileSize': 512,
+    'maxzoom': 14
+    });
+    // add the DEM source as a terrain layer with exaggerated height
+    map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.5 });
+     
+    // add sky styling with `setFog` that will show when the map is highly pitched
+    map.setFog({
+    'horizon-blend': 0.3,
+    'color': '#f8f0e3',
+    'high-color': '#add8e6',
+    'space-color': '#d8f2ff',
+    'star-intensity': 0.0
+    });
+    });
+
 }
 
 mapSetup()
--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const app = express();
 
 // serves up our static files in the public folder
 app.use(express.static('public'));
+// routes the client to the address
+// req, res is apart of the express api, request and response but you can call it whatever
 app.get('/getmapbox', (req, res) =>{
     res.json({token:process.env.ACCESS_TOKEN})
 })
